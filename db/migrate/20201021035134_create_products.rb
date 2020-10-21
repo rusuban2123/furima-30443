1a82c7e93933cb5ded043b47b82d52ca6c1b1fad
rusuban2123 <shin12032123@gmail.com>
2020-10-21 12:59:20 +0900

    productsテーブル作成

--- db/migrate/20201021035134_create_products.rb
+++ db/migrate/20201021035134_create_products.rb
@@ -1,7 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-
+      t.references :user,          null: false, foreign_key: true
+      t.string :name,              null: false
+      t.text :description,         null: false
+      t.integer :category_id,      null: false
+      t.integer :state_id,         null: false
+      t.integer :load_id,          null: false
+      t.integer :shipment_source_id,  null: false
+      t.integer :day_to_ship_id,   null: false
+      t.integer :price,            null: false
       t.timestamps
     end
   end
