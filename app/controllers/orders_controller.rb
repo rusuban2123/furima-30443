6d83fee05b5b5d163f449a2a997070a2b53c4eda
rusuban2123 <shin12032123@gmail.com>
2020-10-29 14:13:30 +0900

    order_indexまでのルーティングとアクション

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :product_params, only: [:index]
+  
+  def index
+  end
+
+  private
+  def product_params
+    @product = Product.find(params[:product_id])
+  end
+
+
 end
+
+
+
